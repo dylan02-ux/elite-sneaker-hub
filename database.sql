a340613d385084dc2c280f63217d5ccd1e180994
Dylan <dylanasaleh@gmail.com>
2026-09-11 14:56:34 +0300

    Replace old sneaker image references

--- database.sql
+++ database.sql
@@ -110,36 +110,36 @@ INSERT INTO categories (name) VALUES
 ('Classic');
 
 INSERT INTO shoes (name, brand, price, size, stock, category_id, image_url) VALUES 
-('Air Jordan 1', 'Nike', 179.99, 10.0, 15, 2, '/qondra.png'),
-('Ultra Boost 21', 'Adidas', 159.99, 9.5, 20, 1, '/qondra.png'),
-('Chuck Taylor', 'Converse', 69.99, 8.5, 25, 3, '/qondra.png'),
-('Lebron 18', 'Nike', 199.99, 11.0, 10, 2, '/qondra.png'),
-('RS-X', 'Puma', 89.99, 9.0, 30, 4, '/qondra.png'),
-('Air Max 270', 'Nike', 149.99, 8.0, 18, 1, '/qondra.png'),
-('Superstar', 'Adidas', 84.99, 7.5, 22, 3, '/qondra.png'),
-('Old Skool', 'Vans', 59.99, 9.0, 28, 8, '/qondra.png'),
-('Gel-Kayano 27', 'ASICS', 159.99, 10.5, 12, 1, '/qondra.png'),
-('Blazer Mid', 'Nike', 99.99, 8.5, 15, 13, '/qondra.png'),
-('NMD R1', 'Adidas', 129.99, 9.5, 20, 13, '/qondra.png'),
-('Rider FV', 'Puma', 79.99, 8.0, 25, 3, '/qondra.png'),
-('Air Force 1', 'Nike', 109.99, 10.0, 30, 13, '/qondra.png'),
-('Stan Smith', 'Adidas', 89.99, 9.0, 35, 3, '/qondra.png'),
-('Classic Leather', 'Reebok', 74.99, 8.5, 18, 30, '/qondra.png'),
-('Zoom Freak', 'Nike', 129.99, 11.0, 15, 2, '/qondra.png'),
-('Continental 80', 'Adidas', 94.99, 9.5, 22, 13, '/qondra.png'),
-('Suede Classic', 'Puma', 69.99, 8.0, 28, 3, '/qondra.png'),
-('Air Zoom Pegasus', 'Nike', 119.99, 10.5, 20, 1, '/qondra.png'),
-('Dame 7', 'Adidas', 139.99, 11.0, 12, 2, '/qondra.png'),
-('Club C 85', 'Reebok', 79.99, 9.0, 25, 30, '/qondra.png'),
-('Kyrie 7', 'Nike', 129.99, 10.0, 18, 2, '/qondra.png'),
-('Ozweego', 'Adidas', 109.99, 8.5, 20, 13, '/qondra.png'),
-('Future Rider', 'Puma', 84.99, 9.5, 30, 13, '/qondra.png'),
-('Metcon 6', 'Nike', 129.99, 10.0, 15, 7, '/qondra.png'),
-('UltraBoost DNA', 'Adidas', 179.99, 9.0, 18, 1, '/qondra.png'),
-('Sky Dreamer', 'Puma', 109.99, 11.0, 20, 2, '/qondra.png'),
-('Crater Impact', 'Nike', 89.99, 8.5, 25, 13, '/qondra.png'),
-('ZX 750', 'Adidas', 99.99, 9.5, 22, 13, '/qondra.png'),
-('Cell Alien', 'Puma', 94.99, 10.0, 28, 13, '/qondra.png');
+('Air Jordan 1', 'Nike', 179.99, 10.0, 15, 2, 'shoe-default.avif'),
+('Ultra Boost 21', 'Adidas', 159.99, 9.5, 20, 1, 'shoe-default.avif'),
+('Chuck Taylor', 'Converse', 69.99, 8.5, 25, 3, 'shoe-default.avif'),
+('Lebron 18', 'Nike', 199.99, 11.0, 10, 2, 'shoe-default.avif'),
+('RS-X', 'Puma', 89.99, 9.0, 30, 4, 'shoe-default.avif'),
+('Air Max 270', 'Nike', 149.99, 8.0, 18, 1, 'shoe-default.avif'),
+('Superstar', 'Adidas', 84.99, 7.5, 22, 3, 'shoe-default.avif'),
+('Old Skool', 'Vans', 59.99, 9.0, 28, 8, 'shoe-default.avif'),
+('Gel-Kayano 27', 'ASICS', 159.99, 10.5, 12, 1, 'shoe-default.avif'),
+('Blazer Mid', 'Nike', 99.99, 8.5, 15, 13, 'shoe-default.avif'),
+('NMD R1', 'Adidas', 129.99, 9.5, 20, 13, 'shoe-default.avif'),
+('Rider FV', 'Puma', 79.99, 8.0, 25, 3, 'shoe-default.avif'),
+('Air Force 1', 'Nike', 109.99, 10.0, 30, 13, 'shoe-default.avif'),
+('Stan Smith', 'Adidas', 89.99, 9.0, 35, 3, 'shoe-default.avif'),
+('Classic Leather', 'Reebok', 74.99, 8.5, 18, 30, 'shoe-default.avif'),
+('Zoom Freak', 'Nike', 129.99, 11.0, 15, 2, 'shoe-default.avif'),
+('Continental 80', 'Adidas', 94.99, 9.5, 22, 13, 'shoe-default.avif'),
+('Suede Classic', 'Puma', 69.99, 8.0, 28, 3, 'shoe-default.avif'),
+('Air Zoom Pegasus', 'Nike', 119.99, 10.5, 20, 1, 'shoe-default.avif'),
+('Dame 7', 'Adidas', 139.99, 11.0, 12, 2, 'shoe-default.avif'),
+('Club C 85', 'Reebok', 79.99, 9.0, 25, 30, 'shoe-default.avif'),
+('Kyrie 7', 'Nike', 129.99, 10.0, 18, 2, 'shoe-default.avif'),
+('Ozweego', 'Adidas', 109.99, 8.5, 20, 13, 'shoe-default.avif'),
+('Future Rider', 'Puma', 84.99, 9.5, 30, 13, 'shoe-default.avif'),
+('Metcon 6', 'Nike', 129.99, 10.0, 15, 7, 'shoe-default.avif'),
+('UltraBoost DNA', 'Adidas', 179.99, 9.0, 18, 1, 'shoe-default.avif'),
+('Sky Dreamer', 'Puma', 109.99, 11.0, 20, 2, 'shoe-default.avif'),
+('Crater Impact', 'Nike', 89.99, 8.5, 25, 13, 'shoe-default.avif'),
+('ZX 750', 'Adidas', 99.99, 9.5, 22, 13, 'shoe-default.avif'),
+('Cell Alien', 'Puma', 94.99, 10.0, 28, 13, 'shoe-default.avif');
 
 INSERT INTO orders (user_id, shoe_id, quantity, status) VALUES 
 (2, 1, 1, 'completed'),
@@ -174,7 +174,7 @@ INSERT INTO orders (user_id, shoe_id, quantity, status) VALUES
 (2, 30, 2, 'completed');
 
 -- Update all shoes to have the same image
-UPDATE shoes SET image_url = '/qondra.png';
+UPDATE shoes SET image_url = 'shoe-default.avif';
 
 -- Create Indexes
 CREATE INDEX idx_shoes_category ON shoes(category_id);
